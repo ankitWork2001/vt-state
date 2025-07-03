360784c5ba4b29f0d0de24bd0e29f58e834a94fb
AMANKUMAR1020 <amankumarchouhan1020@gmail.com>
2025-07-03 11:29:19 +0530

    NavigationMenu updated

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,7 +8,7 @@ import NavigationMenu from '@/components/common/NavigationMenu';
 const Contact = () => {
   return (
     <div className="bg-white">
-      <NavigationMenu />
+      <NavigationMenu path={[{ label: "Home", href: "/" }, { label: "Contact" }]} />
       <ContactBanner />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-[80%] place-self-center">
         <ContactInfo />
