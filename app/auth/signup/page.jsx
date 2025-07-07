6b019e6392709e5af16deb6336c87b13004abcdb
AMANKUMAR1020 <amankumarchouhan1020@gmail.com>
2025-07-07 13:19:49 +0530

    login and signup page

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import OtpInput from '@/components/Auth/OtpInput'
-import LoginForm from '@/components/Auth/LoginForm'
 import SignupForm from '@/components/Auth/SignupForm'
 
 function signup() {
   return (
-    <div>signup</div>
+    <div>
+      <SignupForm/>
+    </div>
   )
 }
 
