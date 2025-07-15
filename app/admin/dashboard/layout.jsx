2a4c6c6fe0799c915ccc7a1b904e8c53a23fd3fa
AMANKUMAR1020 <amankumarchouhan1020@gmail.com>
2025-07-15 17:39:26 +0530

    editor change frontend part

--- app/admin/dashboard/layout.jsx
+++ app/admin/dashboard/layout.jsx
@@ -4,9 +4,9 @@ import ProtectedRoute from '@/components/layout/ProtectedRoute';
 export default function ProfileLayout({ children }) {
   return (
     
-      // <ProtectedRoute checkRoute={'admin'}>       
+      <ProtectedRoute checkRoute={'admin'}>       
       <div className="mt-6">{children}</div>
-      // </ProtectedRoute>
+      </ProtectedRoute>
  
   );
 }
